af921dda88a7b3655456780672aede9e55644362
Stephen Cavaliere <stephencavaliere@gmail.com>
2013-05-30 19:46:41 -0400

    Made script to automate creating a directory. Added problem 2 and 3

---
+++ app.js
@@ -0,0 +1,56 @@
+// THIS WAS ONLY CREATED TO SAVE FROM COPY/PASTE
+// It's sloppy. I wrote it in < 10 minutes. It will have bugs.
+// If you use this, you are hereby warned.
+
+var fs = require('fs')
+  , request = require('request')
+  , cheerio = require('cheerio')
+  , problemNumber = 3;
+
+function createFolder(callback) {
+  var folderName = 'problem-0';
+
+  if (problemNumber < 10) {
+    folderName += '00' + problemNumber;
+  } else if (problemNumber < 100) {
+    folderName += '0' + problemNumber;
+  } else {
+    folderName += problemNumber;
+  }
+
+  fs.mkdir(folderName, 0775, function(err) {
+    if (err) throw err;
+
+    callback(folderName);
+  });
+}
+
+function parsePage(folderName) {
+  var url = "http://www.projecteuler.net/problem=" + problemNumber;
+  request(url, function(err, resp, body){
+    var $ = cheerio.load(body);
+    var problemTitle = $('div#content > h2').text();
+    var problemStatement = $('div.problem_content p');
+
+    var fileToBeWritten = problemTitle + '\n==========\n\n';
+
+    problemStatement.each(function(i, pTag) {
+      fileToBeWritten += $(pTag).text() + '\n\n';
+    });
+
+    fileToBeWritten += '[Link to problem](' + url +')';
+
+    fs.writeFile(folderName + '/README.md', fileToBeWritten, function(err) {
+      if(err) throw err;
+      fs.writeFile(folderName + '/answer.txt', '', function(err) {
+        if (err) throw err;
+      });
+    });
+  });
+}
+
+function makeProblemFolder() {
+  createFolder(parsePage);
+}
+
+makeProblemFolder();
---
+++ problem-0003/README.md
@@ -0,0 +1,8 @@
+Largest prime factor
+==========
+
+The prime factors of 13195 are 5, 7, 13 and 29.
+
+What is the largest prime factor of the number 600851475143 ?
+
+[Link to problem](http://www.projecteuler.net/problem=3)
